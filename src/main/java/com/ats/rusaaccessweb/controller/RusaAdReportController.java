ef77a67f802c92dbd99e7401b9ee8ce083812710
maddy <maddy@maddy-Inspiron-3541>
2019-07-05 11:03:32 +0530

    //maddy

--- src/main/java/com/ats/rusaaccessweb/controller/RusaAdReportController.java
+++ src/main/java/com/ats/rusaaccessweb/controller/RusaAdReportController.java
@@ -32,6 +32,7 @@ import com.ats.rusaaccessweb.common.Constants;
 import com.ats.rusaaccessweb.common.ExceUtil;
 import com.ats.rusaaccessweb.common.ExportToExcel;
 import com.ats.rusaaccessweb.model.CurricularActivityCnts;
+import com.ats.rusaaccessweb.model.dashb.CompetitiveExamReport;
 import com.ats.rusaaccessweb.model.dashb.PlacementUgPgStud;
 import com.ats.rusasoft.util.ItextPageEvent;
 import com.itextpdf.text.BaseColor;
@@ -177,19 +178,19 @@ public class RusaAdReportController {
 
 						cell = new PdfPCell(new Phrase("" + curricular.getCurriCnt(), headFontData));
 						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-						cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+						cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
 
 						table.addCell(cell);
 
 						cell = new PdfPCell(new Phrase("" + curricular.getCoCurryCnt(), headFontData));
 						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-						cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+						cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
 
 						table.addCell(cell);
 
 						cell = new PdfPCell(new Phrase("" + curricular.getExtraCoCarryCnt(), headFontData));
 						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
-						cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+						cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
 
 						table.addCell(cell);
  
@@ -272,6 +273,262 @@ public class RusaAdReportController {
 							rowData.add("" + curriActList.get(i).getCurriCnt());
 							rowData.add("" + curriActList.get(i).getCoCurryCnt());
 							rowData.add("" + curriActList.get(i).getExtraCoCarryCnt());
+							 
+
+							expoExcel.setRowData(rowData);
+							exportToExcelList.add(expoExcel);
+
+						}
+
+						XSSFWorkbook wb = null;
+						try {
+ 
+
+							wb = ExceUtil.createWorkbook(exportToExcelList, "", reportName,
+									"  ", "", 'F');
+
+							ExceUtil.autoSizeColumns(wb, 3);
+							response.setContentType("application/vnd.ms-excel");
+							String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+							response.setHeader("Content-disposition",
+									"attachment; filename=" + reportName + "-" + date + ".xlsx");
+							wb.write(response.getOutputStream());
+
+						} catch (IOException ioe) {
+							throw new RuntimeException("Error writing spreadsheet to output stream");
+						} finally {
+							if (wb != null) {
+								wb.close();
+							}
+						}
+
+					}
+
+				} catch (DocumentException ex) {
+
+					// System.out.println("Pdf Generation Error: " + ex.getMessage());
+
+					ex.printStackTrace();
+
+				}
+
+			} catch (Exception e) {
+
+				System.err.println("Exce in showProgReport " + e.getMessage());
+				e.printStackTrace();
+
+			}
+
+		}
+	
+	@RequestMapping(value = "/getCompitetiveExmReport", method = RequestMethod.POST)
+	public void getCompitetiveExmReport(HttpServletRequest request, HttpServletResponse response) {
+		 
+
+			String reportName = "Competitive Exam(No. of Student appeared for Conpetitive Exam at various levels)";
+
+		 
+			try {
+				map = new LinkedMultiValueMap<String, Object>();
+				int yearId = 12;
+				map.add("yearId", yearId);
+ 
+				CompetitiveExamReport[] resArray = Constants.getRestTemplate().postForObject(Constants.url + "getCompetitiveExamReport", map,
+						CompetitiveExamReport[].class);
+				List<CompetitiveExamReport> competExmList = new ArrayList<>(Arrays.asList(resArray));
+ 
+				Document document = new Document(PageSize.A4);
+				document.setMargins(50, 45, 50, 60);
+				document.setMarginMirroring(false);
+ 
+				String FILE_PATH = Constants.REPORT_SAVE;
+				File file = new File(FILE_PATH);
+
+				PdfWriter writer = null;
+
+				FileOutputStream out = new FileOutputStream(FILE_PATH);
+				try {
+					writer = PdfWriter.getInstance(document, out);
+				} catch (DocumentException e) {
+
+					e.printStackTrace();
+				}
+
+				String header = "";
+				String title = "                 ";
+
+				DateFormat DF2 = new SimpleDateFormat("dd-MM-yyyy");
+				String repDate = DF2.format(new Date());
+
+				 
+				ItextPageEvent event = new ItextPageEvent(header, title, "",""  );
+
+				writer.setPageEvent(event);
+				// writer.add(new Paragraph("Curricular Aspects"));
+
+				PdfPTable table = new PdfPTable(5);
+
+				table.setHeaderRows(1);
+
+				try {
+					table.setWidthPercentage(100);
+					table.setWidths(new float[] { 1.5f, 5.9f, 2.3f, 2.3f, 2.3f});
+					Font headFontData = Constants.headFontData;// new Font(FontFamily.TIMES_ROMAN, 12, Font.NORMAL,
+																// BaseColor.BLACK);
+					Font tableHeaderFont = Constants.tableHeaderFont; // new Font(FontFamily.HELVETICA, 12, Font.BOLD,
+																		// BaseColor.BLACK);
+					tableHeaderFont.setColor(Constants.tableHeaderFontBaseColor);
+
+					PdfPCell hcell = new PdfPCell();
+					hcell.setBackgroundColor(BaseColor.LIGHT_GRAY);
+
+					hcell = new PdfPCell(new Phrase("Sr.No.", tableHeaderFont));
+					hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+					hcell.setBackgroundColor(Constants.baseColorTableHeader);
+
+					table.addCell(hcell);
+
+					hcell = new PdfPCell(new Phrase("Institute Name", tableHeaderFont));
+					hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+					hcell.setBackgroundColor(Constants.baseColorTableHeader);
+
+					table.addCell(hcell);
+
+					hcell = new PdfPCell(new Phrase("State Level", tableHeaderFont));
+					hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+					hcell.setBackgroundColor(Constants.baseColorTableHeader);
+
+					table.addCell(hcell);
+
+					hcell = new PdfPCell(new Phrase("National Level", tableHeaderFont));
+					hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+					hcell.setBackgroundColor(Constants.baseColorTableHeader);
+
+					table.addCell(hcell);
+
+					hcell = new PdfPCell(new Phrase("International Level", tableHeaderFont));
+					hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+					hcell.setBackgroundColor(Constants.baseColorTableHeader);
+
+					table.addCell(hcell);
+
+				 
+
+					int index = 0;
+					for (int i = 0; i < competExmList.size(); i++) {
+						// System.err.println("I " + i);
+						CompetitiveExamReport competExm = competExmList.get(i);
+
+						index++;
+						PdfPCell cell;
+						cell = new PdfPCell(new Phrase(String.valueOf(index), headFontData));
+						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+						cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+
+						table.addCell(cell);
+
+						cell = new PdfPCell(new Phrase("" + competExm.getInstituteName(), headFontData));
+						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+						cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+
+						table.addCell(cell);
+
+						cell = new PdfPCell(new Phrase("" + competExm.getStateLevelAppear(), headFontData));
+						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+						cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+
+						table.addCell(cell);
+
+						cell = new PdfPCell(new Phrase("" + competExm.getNationalLevelAppear(), headFontData));
+						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+						cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+
+						table.addCell(cell);
+
+						cell = new PdfPCell(new Phrase("" + competExm.getIntLevelAppear(), headFontData));
+						cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+						cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+
+						table.addCell(cell);
+			
+					}
+
+					document.open();
+					Font hf = new Font(FontFamily.TIMES_ROMAN, 12.0f, Font.UNDERLINE, BaseColor.BLACK);
+
+					Paragraph name = new Paragraph(reportName, hf);
+					name.setAlignment(Element.ALIGN_LEFT);
+					document.add(name);
+					document.add(new Paragraph("\n"));
+					 
+					DateFormat DF = new SimpleDateFormat("dd-MM-yyyy");
+					 
+					document.add(table);
+
+					int totalPages = writer.getPageNumber();
+
+					// System.out.println("Page no " + totalPages);
+
+					document.close();
+					int p = Integer.parseInt(request.getParameter("p"));
+					System.err.println("p " + p);
+
+					if (p == 1) {
+
+						if (file != null) {
+
+							String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+
+							if (mimeType == null) {
+
+								mimeType = "application/pdf";
+
+							}
+
+							response.setContentType(mimeType);
+
+							response.addHeader("content-disposition",
+									String.format("inline; filename=\"%s\"", file.getName()));
+
+							response.setContentLength((int) file.length());
+
+							InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+							try {
+								FileCopyUtils.copy(inputStream, response.getOutputStream());
+							} catch (IOException e) {
+								// System.out.println("Excep in Opening a Pdf File");
+								e.printStackTrace();
+							}
+						}
+					} else {
+
+						List<ExportToExcel> exportToExcelList = new ArrayList<ExportToExcel>();
+
+						ExportToExcel expoExcel = new ExportToExcel();
+						List<String> rowData = new ArrayList<String>();
+
+						rowData.add("Sr. No");
+						rowData.add("Institute Name");
+						rowData.add("State Level");
+						rowData.add("National Level");
+						rowData.add("International Level");
+					 
+
+						expoExcel.setRowData(rowData);
+						exportToExcelList.add(expoExcel);
+						int cnt = 1;
+						for (int i = 0; i < competExmList.size(); i++) {
+							expoExcel = new ExportToExcel();
+							rowData = new ArrayList<String>();
+							cnt = cnt + i;
+
+							rowData.add("" + (i + 1));
+
+							rowData.add("" + competExmList.get(i).getInstituteName());
+							rowData.add("" + competExmList.get(i).getStateLevelAppear());
+							rowData.add("" + competExmList.get(i).getNationalLevelAppear());
+							rowData.add("" + competExmList.get(i).getIntLevelAppear());
 							 
 
 							expoExcel.setRowData(rowData);
